7f4b99f4f1781cbd8f317d48e27ea5fed7e26828
mohammadalisiddiqui <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-02 18:12:25 +0500

    Add files via upload

---
+++ seven_segment.ino
@@ -0,0 +1,49 @@
+int a=12;
+int b=11;
+int c=10;
+int d=9;
+int e=8;
+int f=7;
+int g=6;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+  delay(1000);
+// 2
+    digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);
+  delay(1000);
+//    3
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);
+  delay(1000);
+//  ali
+
+}
