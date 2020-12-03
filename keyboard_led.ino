eb24d3e8ff527e2aa5de446a5425abbcc635f00f
mohammadalisiddiqui <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-03 18:18:14 +0500

    Add files via upload

---
+++ keyboard_led.ino
@@ -0,0 +1,19 @@
+char a;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Enter 1 for glow led and 0 for off");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ Serial.available();
+ a=Serial.read();
+ if(a=='1'){
+  digitalWrite(LED_BUILTIN,HIGH);
+  }
+  else if(a=='0'){
+    digitalWrite(LED_BUILTIN,LOW);
+    }
+}
