a6e7ebb68d272ba07811f7907e10990d59465a6e
Ali <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-04 19:42:32 +0500

    Add files via upload

---
+++ analog_voltmeter.ino
@@ -0,0 +1,13 @@
+float y;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ int x=analogRead(A1);
+ y=(x/1023.0)*5.0;
+ Serial.println(y);
+ delay(1000);
+}
