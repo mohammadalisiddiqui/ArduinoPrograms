06caa1a092658ca91fda12d337578913d6f42f70
Ali <36238347+mohammadalisiddiqui@users.noreply.github.com>
2020-12-11 05:29:24 -0800

    11 Dec,2020
    Servo Motor

---
+++ Oscilloscope.ino
@@ -0,0 +1,29 @@
+const int pwm = 9;
+const int AnalogIn = A0;
+int i;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pwm, OUTPUT);
+  pinMode(AnalogIn, INPUT);
+}
+
+void loop() {
+ for(i=0; i<=255; i++) {
+  analogWrite(pwm, i);
+  int value = analogRead(AnalogIn);
+  Serial.println(value);
+  delay(500);
+ }
+
+ for(i=255; i>=0; i--) {
+  analogWrite(pwm, i);
+  int value = analogRead(AnalogIn);
+  Serial.println(value);
+  delay(500);
+ }
+
+ 
+}
+
+
---
+++ Servo.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x = analogRead(A0);
+  int y = map(x,0,1023,0,255);
+  analogWrite(3,y);
+  Serial.println(y);
+  delay(100);
+}
